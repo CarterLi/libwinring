eb41d00b7723839071bea79ab699436e5ad77c90
李通洲 <carter.li@eoitek.com>
2022-07-07 23:49:53 +0800

    Tidy

--- test/debugInfo.cpp
+++ test/debugInfo.cpp
@@ -5,7 +5,7 @@ int main() {
 
     printf("IoRing Version: %d\n", (int)capabilities.IoRingVersion);
     printf("Max opcode: %d\n", capabilities.MaxOpCode);
-    printf("Supported flags: %x\n\n", capabilities.FlagsSupported);
-    printf("Submission queue size: %u\n\n", capabilities.SubmissionQueueSize);
-    printf("Completion queue size: %u\n\n", capabilities.CompletionQueueSize);
+    printf("Supported flags: %x\n", capabilities.FlagsSupported);
+    printf("Submission queue size: %u\n", capabilities.SubmissionQueueSize);
+    printf("Completion queue size: %u\n", capabilities.CompletionQueueSize);
 }
