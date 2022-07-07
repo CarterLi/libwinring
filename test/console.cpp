9a694406482fc566a438dc504d6590681d071282
李通洲 <carter.li@eoitek.com>
2022-07-07 23:47:03 +0800

    Make test/console working

--- test/console.cpp
+++ test/console.cpp
@@ -1,13 +1,25 @@
 #include "common.hpp"
 
+#include <string_view>
+
 int main() {
+    using namespace std::literals;
+
     win_ring_cpp ring(32);
 
-    char str[128] = "";
+    char str[512] = "";
+
+    HANDLE hConIn = GetStdHandle(STD_INPUT_HANDLE);
+    if (hConIn == INVALID_HANDLE_VALUE) panic();
+    HANDLE hConOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hConOut == INVALID_HANDLE_VALUE) panic();
 
     ring.get_sqe()
-        ->prep_read(CONSOLE_REAL_INPUT_HANDLE, str, sizeof str, 0, NT_IORING_OP_FLAG_NONE);
-    ring.submit_and_wait(1);
+        ->prep_read( hConIn, str, sizeof (str), 0, NT_IORING_OP_FLAG_NONE);
+    ring.get_sqe()
+        ->prep_write(hConOut, str, sizeof (str), 0, FILE_WRITE_FLAGS_NONE, NT_IORING_OP_FLAG_NONE)
+        ->set_flags(NT_IORING_SQE_FLAG_DRAIN_PRECEDING_OPS);
+    ring.submit_and_wait(2);
 
-    clear_cqes(&ring, "read");
+    clear_cqes(&ring, "console");
 }
